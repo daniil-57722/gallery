c225679d2adf1a79fddcddf93bf78b21031733fb
Daniil <penzadaniilfilippov99@mail.ru>
2021-12-09 11:06:27 +0300

    сразу с поиском картинок по устройству

---
+++ app/src/main/java/ru/example/mygallery/MainActivity.java
@@ -0,0 +1,94 @@
+package ru.example.mygallery;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    ArrayList<Cell> allFilesPaths;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M &&
+                checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) !=
+                        PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1000);
+        } else {
+            showImages();
+        }
+    }
+
+    private void showImages() {
+
+        Uri allImagesuri = android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+        String[] projection = { MediaStore.Images.ImageColumns.DATA ,MediaStore.Images.Media.DISPLAY_NAME,
+                MediaStore.Images.Media.BUCKET_DISPLAY_NAME,MediaStore.Images.Media.BUCKET_ID};
+        Cursor cursor = this.getContentResolver().query(allImagesuri, projection, null, null, null);
+        try {
+            int count = 0;
+            allFilesPaths = new ArrayList<>();
+            if (cursor != null) {
+                cursor.moveToLast();
+            }
+            do{
+                Cell path = new Cell();
+                path.setTitle(cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DISPLAY_NAME)));
+                path.setPath(cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA)));
+                count++;
+                allFilesPaths.add(path);}
+            while(cursor.moveToPrevious()&&count<100);
+        cursor.close();
+    }
+        catch (Exception e) {
+            e.printStackTrace();
+        }for(int i = 0;i < allFilesPaths.size();i++){
+            Log.d("picture folders",allFilesPaths.get(i).getTitle()+" and path = "+allFilesPaths.get(i).getPath());
+        }
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.gallery);
+        recyclerView.setNestedScrollingEnabled(false);
+        recyclerView.setHasFixedSize(true);
+        // список в три колонки
+        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(getApplicationContext(), 3);
+        recyclerView.setLayoutManager(layoutManager);
+
+        //оптимизация
+        recyclerView.setItemViewCacheSize(20);
+        recyclerView.setDrawingCacheEnabled(true);
+        recyclerView.setDrawingCacheQuality(View.DRAWING_CACHE_QUALITY_HIGH);
+
+        MyAdapter adapter = new MyAdapter(getApplicationContext(), allFilesPaths);
+        recyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 1000) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                showImages();
+            } else {
+                Toast.makeText(this, "Разрешения на чтение нет", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        }
+    }
+}
